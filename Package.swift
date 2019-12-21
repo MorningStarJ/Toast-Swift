bcf82cb194f4761fdf54f398a87c3a694300c4e7
MorningStar <jngviho@icloud.com>
2019-12-21 17:53:51 +0800

    fix: 修复Sources 路径问题

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Toast-Swift",
-            targets: ["Toast-Swift"]),
+            name: "Toast",
+            targets: ["Toast"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Toast-Swift",
+            name: "Toast",
             dependencies: []),
-        .testTarget(
-            name: "Toast-SwiftTests",
-            dependencies: ["Toast-Swift"]),
     ]
 )
